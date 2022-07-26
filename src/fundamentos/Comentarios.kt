ec993fcbeeaf9f8a2316f016da231a57010113f6
Gabriel Novaes <gabrielnovaes@yahoo.com.br>
2022-07-26 13:33:17 -0300

    initial example how to create main function and comments

---
+++ src/fundamentos/Comentarios.kt
@@ -0,0 +1,14 @@
+package fundamentos
+
+/**
+ * Comentario usando KDoc
+ * @param args lista de parametros passados por linha de comando
+ * @author Gabriel Novaes
+ */
+fun main() {
+    //comentario de uma linha
+
+    /*
+    * comentario com multiplas linhas
+    * */
+}
