e2ed61be0d374f0b119c643ffa0383c7d5b4edfa
Gabriel Novaes <gabrielnovaes@yahoo.com.br>
2022-07-27 17:49:55 -0300

    example template string 1

---
+++ src/fundamentos/TemplateString1.kt
@@ -0,0 +1,7 @@
+package fundamentos
+
+fun main(args: Array<String>) {
+    val aprovados = listOf<String>("Joao","Maria","Roberto","Renan","Daniela","Henrique")
+
+    println("O primeiro colocado foi: ${aprovados[3]} .")
+    }
