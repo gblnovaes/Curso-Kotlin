cea5631b02da07795c8294a3e3bc4fd309c7634e
Gabriel Novaes <gabrielnovaes@yahoo.com.br>
2022-08-01 19:47:35 -0300

    command if basics examples

---
+++ src/fundamentos/controles/IfElse.kt
@@ -0,0 +1,11 @@
+package fundamentos.controles
+
+fun main() {
+     val nota = 1.3
+
+     if(nota >=  7.0){
+          println("Aprovado")
+     }else{
+          println("Reprovado")
+     }
+}
---
+++ src/fundamentos/controles/IfElse2.kt
@@ -0,0 +1,4 @@
+package fundamentos.controles
+
+class IfElse2 {
+}
