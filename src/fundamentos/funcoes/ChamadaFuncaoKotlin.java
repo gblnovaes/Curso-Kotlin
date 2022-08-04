0d335bddfee4f0046e33c585b9a29ec7b3e1b228
Gabriel Novaes <gabrielnovaes@yahoo.com.br>
2022-08-04 18:46:40 -0300

    chamada de funcao kotlin em java

---
+++ src/fundamentos/funcoes/ChamadaFuncaoKotlin.java
@@ -0,0 +1,13 @@
+package fundamentos.funcoes;
+
+import kotlin.collections.CollectionsKt;
+
+import java.util.ArrayList;
+
+public class ChamadaFuncaoKotlin {
+    public static void main(String[] args) {
+        ArrayList<String> list = CollectionsKt.arrayListOf("Joao", "Maria", "Pedro");
+
+        System.out.println(SegundoElementoListKt.secondOrNull(list));
+    }
+}
