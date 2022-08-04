1d70d427e792e1ada1439a19805642c1be0a55c7
Gabriel Novaes <gabrielnovaes@yahoo.com.br>
2022-08-04 18:14:37 -0300

    argumentos nomeados

---
+++ src/fundamentos/funcoes/ArgsNomeados.kt
@@ -0,0 +1,11 @@
+package fundamentos.funcoes
+
+
+fun relacaoDeTrabalho(chefe : String,funcionario : String): String {
+    return "$funcionario e subordinado a $chefe"
+}
+
+fun main() {
+    println(relacaoDeTrabalho("Joao","Maria"))
+    println(relacaoDeTrabalho(funcionario = "Renato", chefe = "Maria"))
+}
