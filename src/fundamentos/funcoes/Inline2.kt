ee0915796ed7473a3abc01165cdcb12aa59f257e
Gabriel Novaes <gabrielnovaes@yahoo.com.br>
2022-08-05 19:33:44 -0300

    exemplo de funcao inline 2

---
+++ src/fundamentos/funcoes/Inline2.kt
@@ -0,0 +1,22 @@
+package fundamentos.funcoes
+
+inline fun <T>executarComLog(nome : String,funcao : ()-> T) : T {
+    println("Entrando no metodo $nome")
+    try {
+        return funcao()
+    }finally {
+        println("Metodo $nome finalizado")
+    }
+}
+
+fun somar(a: Int , b : Int) :Int {
+    return a+b
+}
+
+fun main() {
+    val resultado = executarComLog("somar"){
+        somar(2,5)
+    }
+
+    println(resultado)
+}
