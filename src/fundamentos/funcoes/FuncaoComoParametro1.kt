902efcf41717a88bce4d2dc857b79b729fdc3fe1
Gabriel Novaes <gabrielnovaes@yahoo.com.br>
2022-08-05 17:56:12 -0300

    funcao como parametro

---
+++ src/fundamentos/funcoes/FuncaoComoParametro1.kt
@@ -0,0 +1,19 @@
+package fundamentos.funcoes
+
+ class Operacoes{
+     fun soma(a:Int,b:Int):Int{
+         return a + b
+     }
+ }
+fun soma(a:Int,b:Int):Int{
+    return a + b
+}
+
+fun calc(a :Int,b : Int,funcao : (Int,Int) ->Int) : Int {
+    return funcao(a,b)
+}
+
+fun main() {
+    println(calc(2,3,Operacoes()::soma))
+    println(calc(2,3,::soma))
+}
