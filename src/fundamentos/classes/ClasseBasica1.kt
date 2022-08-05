8ada36d82186ed7c21c88bb7c07331b8d038e83f
Gabriel Novaes <gabrielnovaes@yahoo.com.br>
2022-08-05 19:44:29 -0300

    classe basica simples

---
+++ src/fundamentos/classes/ClasseBasica1.kt
@@ -0,0 +1,13 @@
+package fundamentos.classes
+
+
+class Cliente{
+    var nome : String = ""
+}
+
+fun main() {
+    val cliente = Cliente()
+    cliente.nome = "Ribeiro Matheus"
+
+    println("O cliente se chama ${cliente.nome}")
+}
