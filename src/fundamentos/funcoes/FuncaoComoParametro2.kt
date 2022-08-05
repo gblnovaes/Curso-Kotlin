76025de1d814d028887a8f62523d357c376286ad
Gabriel Novaes <gabrielnovaes@yahoo.com.br>
2022-08-05 18:50:05 -0300

    funcao com parametros e generics

---
+++ src/fundamentos/funcoes/FuncaoComoParametro2.kt
@@ -0,0 +1,21 @@
+package fundamentos.funcoes
+
+fun <E>filtrar(lista: List<E>,filtro : (E) -> Boolean ) : List<E> {
+    val listaFiltrada  = ArrayList<E>()
+    for(e in lista){
+        if(filtro(e)){
+            listaFiltrada.add(e)
+        }
+    }
+    return listaFiltrada
+}
+
+
+fun comTresLetras(nome : String) : Boolean{
+    return nome.length == 3
+}
+
+fun main() {
+    val listaNome = listOf("Ana","Pedro","Gui","Bia","Rebeca")
+    println(filtrar(listaNome,::comTresLetras))
+}
