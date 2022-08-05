70ed30e117ad69a5cf5c8b234ba6555a5b1b72e8
Gabriel Novaes <gabrielnovaes@yahoo.com.br>
2022-08-05 19:13:31 -0300

    exemplo de funcao inline 1

---
+++ src/fundamentos/funcoes/Inline.kt
@@ -0,0 +1,21 @@
+package fundamentos.funcoes
+
+
+inline fun transacao(funcao: () -> Unit) {
+
+    println("abrindo transacao")
+    try {
+        funcao()
+    }catch (e : Exception){
+        println("fechando transacao")
+    }
+}
+
+fun main() {
+    transacao {
+        println("Executando SQL 1 ....")
+        println("Executando SQL 2 ....")
+        println("Executando SQL 3 ....")
+        println("Executando SQL 4 ....")
+    }
+}
