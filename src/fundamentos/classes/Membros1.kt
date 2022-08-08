4297f4d833ae26105c6115e80f162f0021046786
Gabriel Novaes <gabrielnovaes@yahoo.com.br>
2022-08-08 12:08:55 -0300

    criado classe para exemplificar o uso de membros na class

---
+++ src/fundamentos/classes/Membros1.kt
@@ -0,0 +1,12 @@
+package fundamentos.classes
+
+ data class Data(var dia:Int,var mes:Int,var ano : Int){
+   fun formatar() : String{
+   return "$dia/$mes/$ano"
+  }
+ }
+
+fun main() {
+ var nascimento = Data(dia = 11, mes = 10, ano = 2018)
+ println(nascimento.formatar())
+}
