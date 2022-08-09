a4e677a41cf3adc1b2861df22b14ddea1058e076
Gabriel Novaes <gabrielnovaes@yahoo.com.br>
2022-08-09 18:02:10 -0300

    criado construtor no estilo java

---
+++ src/fundamentos/classes/Construtor1.kt
@@ -0,0 +1,19 @@
+package fundamentos.classes
+
+
+class Filme {
+    val nome: String
+    val anoLancamento: Int
+    val genero: String
+
+    constructor(nome: String, anoLancamento: Int, genero: String = "Nao Informado") {
+        this.genero = genero
+        this.nome = nome
+        this.anoLancamento = anoLancamento
+    }
+}
+
+fun main() {
+    val filme = Filme(nome = "Poderoso Chefão", anoLancamento = 1972, genero = "Drama")
+    println("${filme.nome} foi lancado em ${filme.anoLancamento} ")
+}
