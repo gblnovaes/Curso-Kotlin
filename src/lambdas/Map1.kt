8cc2cc7c6992debfe7ee5afc5a668e2e49ed5e68
Gabriel Novaes <gabrielnovaes@yahoo.com.br>
2022-08-11 18:20:38 -0300

    exemplo de map simples

---
+++ src/lambdas/Map1.kt
@@ -0,0 +1,13 @@
+package lambdas
+
+
+
+fun main() {
+    val alunos = arrayListOf("Pedro","Tiago","Jonas")
+
+    alunos.map {
+        it.uppercase().apply {
+            println(this)
+        }
+    }
+}
