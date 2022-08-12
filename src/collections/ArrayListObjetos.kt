2ea6e61812e9751311de5a27d127761b22f8d3b6
Gabriel Novaes <gabrielnovaes@yahoo.com.br>
2022-08-12 17:20:13 -0300

    exemplo de array list com objetos

---
+++ src/collections/ArrayListObjetos.kt
@@ -0,0 +1,10 @@
+package collections
+
+data class Fruta(var nome : String, var preco : Double)
+fun main() {
+    var frutas = arrayListOf(Fruta("Banana",1.50),Fruta("Morango",3.20))
+
+    for (fruta in frutas){
+        println("${fruta.nome} - ${fruta.preco}" )
+    }
+}
