95a52a9687f363f8a126578fecc2a650424c269b
Gabriel Novaes <gabrielnovaes@yahoo.com.br>
2022-08-12 16:36:10 -0300

    exemplo de como usar Array

--- src/collections/Arrays.kt
+++ src/collections/Arrays.kt
@@ -1,4 +1,16 @@
 package collections
 
-class Arrays {
+fun main() {
+    val numeros = Array(10) { i -> i * 10 }
+
+
+    for (numero in numeros) {
+        println(numero)
+    }
+    println(numeros.get(1))
+    println(numeros[1])
+    println(numeros.size)
+
+    numeros.set(1,1023)
+    println(numeros[1])
 }
